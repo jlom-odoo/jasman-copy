6451d517244d3864c22dfdee143157a5419edc3c
road-odoo <road@odoo.com>
2024-03-12 12:49:54 -0600

    [ADD][3780442]jasman_stock: Made lost_stock.expiration_date depend on lost_stock.lot_creation_date

---
+++ jasman_stock/models/stock_lot.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+from odoo import api, fields, models
+
+class StockMoveLot(models.Model):
+    _inherit = 'stock.lot'
+
+    dot_id = fields.Char()
+    lot_creation_date = fields.Date(compute='_compute_lot_creation_date')
+    name = fields.Char(compute='_compute_name', store=True)
+
+    @api.depends('product_id','lot_creation_date')
+    def _compute_expiration_date(self):
+        super()._compute_expiration_date()
+        for lot in self:
+            if lot.product_id.use_expiration_date and lot.lot_creation_date:
+                duration = lot.product_id.product_tmpl_id.expiration_time
+                lot.expiration_date = lot.lot_creation_date + timedelta(days=duration)
+
+    @api.depends('dot_id', 'product_id.default_code')
+    def _compute_name(self):
+        for lot in self:
+            lot.name = f'{lot.dot_id or "0000"}-{lot.product_id.default_code}'
+
+    @api.depends('dot_id')
+    def _compute_lot_creation_date(self):
+        for lot in self:
+            lot.lot_creation_date = datetime.strptime(f"{lot.dot_id}-1", "%W%y-%w") if lot.dot_id else None
---
+++ jasman_stock/models/stock_move_line.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    dot_id = fields.Char(related='lot_id.dot_id')
+    lot_creation_date = fields.Date(related='lot_id.lot_creation_date')
+
+    @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date')
+    def _compute_expiration_date(self):
+        super()._compute_expiration_date()
+        for move_line in self:
+            if move_line.lot_id.expiration_date:
+                move_line.expiration_date = move_line.lot_id.expiration_date
