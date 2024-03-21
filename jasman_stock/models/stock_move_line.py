a8b3ab68815657c62f255759dd3c690cee698ed9
road-odoo <141050268+road-odoo@users.noreply.github.com>
2024-03-21 15:01:28 -0600

    Update jasman_stock/models/stock_move_line.py
    Co-authored-by: jefe-odoo <91212505+jefe-odoo@users.noreply.github.com>

--- jasman_stock/models/stock_move_line.py
+++ jasman_stock/models/stock_move_line.py
@@ -11,6 +11,9 @@ class StockMoveLine(models.Model):
 
     @api.depends('product_id', 'picking_type_use_create_lots', 'lot_id.expiration_date')
     def _compute_expiration_date(self):
+    super()._compute_expiration_date()
+        for move_line in self.filtered(lambda _move_line: _move_line.lot_id.expiration_date):
+                move_line.expiration_date = move_line.lot_id.expiration_date
         super()._compute_expiration_date()
         for move_line in self:
             if move_line.lot_id.expiration_date:
